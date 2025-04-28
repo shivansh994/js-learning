930cbddfc7875b5746208fedbc2626ca64ab8c59
shivansh994 <117643354+shivansh994@users.noreply.github.com>
2025-04-28 14:10:35 +0000

    Strings

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,18 @@
+const name = "Shivansh";
+const repoCount = 50;
+
+console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Shivansh');
+console.log(gameName);
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('h'));
+
+
---
+++ 01_basics/06_nums-maths.js
@@ -0,0 +1,21 @@
+const score = 400
+console.log(score);
+// console.log("shivansh");
+const balance = new Number(100);
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966;
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000;
+console.log(hundreds.toLocaleString('en-IN'));
+ console.log(Math.random());
+ console.log((Math.random()*10)+1);
+ console.log((Math.random()*10)+1);
+ const min = 10;
+ const max = 20;
+ console.log(Math.floor(Math.random()* (max - min + 1)) + min);
+ 
+ 
